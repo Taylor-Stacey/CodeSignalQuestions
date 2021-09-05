075a07d773e1829a4df2f65c3d0624305a70b2bb
Vippsi <jonathanthornton1997@gmail.com>
2021-09-05 09:21:46 -0700

    update

--- gitpush.js
+++ gitpush.js
@@ -1,52 +1,98 @@
 const fs = require('fs');
+const path = require('path');
 const moment = require('moment');
 const simpleGit = require('simple-git');
 
-
 let projectsDir = './out/arcade/code-arcade';
 
-fs.readdir(projectsDir, function (err, files) {
-  if (err) {
-    console.error('could not list dir', err);
+function getFileSync(dir, files = []) {
+  const listing = fs.readdirSync(dir, { withFileTypes: true });
+  let dirs = [];
+
+  for (let f of listing) {
+    const fullName = path.join(dir, f.name);
+    if (f.isFile()) {
+      files.push(fullName);
+    } else if (f.isDirectory()) {
+      dirs.push(fullName);
+    }
   }
+  for (let d of dirs) {
+    getFileSync(d, files);
+  }
+  return dirs;
+}
 
-  
+let files = getFileSync(projectsDir);
 
-  subby = [];
-  files.forEach((file) => {
-    subby.push(projectsDir + '/' + file);
-  });
-
-  subby.forEach((question) => {
-    fs.readdir(question, function(err, i) {
-        if (err) {
-            console.error('could not list dir', err);
-          }
-        
-        pathArr = []
-        i.forEach(path => {
-            pathArr.push(question + '/' + path)
-        })
-        
-        // count = pathArr.length
-        // console.log(count)
-
-        pathArr.forEach(path => {
-
-            pushCode(path)
-        })
-        
-    })
-  })
+// gets sub directories of parent folder
+let pathArr = [];
+files.forEach((file) => {
+  let yeet = getFileSync(file);
+  pathArr.push(yeet);
 });
 
-counter = 30
-
-function pushCode(path) {
-  let date = moment().subtract(counter, 'd').format()
-
-  simpleGit().add([path]).commit(date, {'--date': date}).push()
-
-  counter -= 1
-
+for (let i = 0; i< pathArr.length, i ++;) {
+  for (let j = 0; j < pathArr[i].length, j ++;){
+    console.log(pathArr[j])
+    // gitPush(pathArr[i][j])
+  }
 }
+
+pathArr.every((subPath) => {
+  // longMan +=  subPath.length
+
+
+  // subPath.every((folder) => {
+  //   gitPush(folder)
+
+  // });
+});
+
+ function gitPush(folder) {
+  let longMan = 40;
+
+  let date = moment().subtract(longMan, 'd').format();
+
+   simpleGit().add([folder]).commit(folder, { '--date': date }).push();
+  longMan -= 1;
+}
+// function GetFinalPaths(projectsDir) {
+//   fs.readdirSync(projectsDir, function (err, files) {
+//     if (err) {
+//       console.error('could not list dir', err);
+//     }
+
+//     subby = [];
+//     files.forEach((file) => {
+//       subby.push(projectsDir + '/' + file);
+//     });
+
+//     subby.forEach((question) => {
+//       fs.readdirSync(question, function (err, i) {
+//         if (err) {
+//           console.error('could not list dir', err);
+//         }
+
+//         pathArr = [];
+//         i.forEach((path) => {
+//           pathArr.push(question + '/' + path);
+//         });
+//         console.log(pathArr);
+//         return pathArr;
+//       });
+//     });
+//   });
+// }
+
+// console.log(GetFinalPaths(projectsDir));
+
+// console.log(paths);
+
+// counter = 30;
+
+// function pushCode(path) {
+//  ();
+
+//   counter -= 1;
+// }
