d097c43c63cdd0a754d8e7e43ef0499788b7b273
Jonathan <49133849+Vippsi@users.noreply.github.com>
2021-09-07 20:38:24 -0700

    Delete code-arcade directory

--- code-arcade/loop-tunnel/Least Factorial/solution.py
+++
@@ -1,7 +0,0 @@
-def leastFactorial(n):
-    if n <= 1:
-        return 1
-    
-    for i in range(10):
-        if math.factorial(i) >= n:
-            return math.factorial(i)
--- code-arcade/loop-tunnel/Rounders/solution.py
+++
@@ -1,7 +0,0 @@
-def rounders(n):
-    x = 1
-    while n > 10:
-        n = (n + 5) // 10
-        print(n)
-        x *= 10
-    return x * n
